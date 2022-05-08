09059e8cc280e55fa75084c6c77b2e4ad7bd7a4c
Mr-Chen-zihao <1636806534@qq.com>
2022-05-08 16:29:45 +0800

    first commit

---
+++ homework11/homework11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+
+                using (var db = new OrderContext())
+                {
+                    var order = db.OrderItems.FirstOrDefault();
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            //Console.ReadKey();
+
+        }
+    }
+}
+
